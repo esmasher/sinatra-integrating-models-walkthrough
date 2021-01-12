3d55aa06eabccf649f04e26bb58c625dcd40c904
esmasher <esmasherr@gmail.com>
2021-01-12 13:20:25 -0600

    commit tree ae13a72fc943d86027a972649d05eeeda3aafd88

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
-
 class App < Sinatra::Base
+  set :views, "views"
+
   get '/' do
     erb :index
   end
 
+
   post '/' do
-    text_from_user = params[:user_text]
-    @analyzed_text = TextAnalyzer.new(text_from_user)
-    erb :results
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+
+    erb :views/results
   end
 end
